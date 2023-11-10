a441bf744de1beccd329a7acdc05328021480a28
Rishabh Singh <rishabh15112001@gmail.com>
2023-11-11 00:00:37 +0530

    Recommendation done

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,53 @@
-import { FlatList } from "react-native";
-import { tracks } from "../../../assets/data/tracks";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import TrackListItem from "../../components/TrackListItem";
+import { gql, useQuery } from "@apollo/client";
+import { genres } from "../../../assets/data/genre";
+
+const getRandomGenre = () => {
+  const randomIndex = Math.floor(Math.random() * genres.length);
+  return genres[randomIndex];
+};
+
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        uri
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            url
+            height
+            width
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function HomeScreen() {
+  const randomGenre = getRandomGenre();
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: randomGenre },
+  });
+
+  if (loading) {
+    return <ActivityIndicator style={{ flex: 1 }} />;
+  }
+  if (error) {
+    return <Text>Failed to fetch recommendations. {error.message}</Text>;
+  }
+  const tracks = data?.recommendations?.tracks || [];
+
   return (
     <FlatList
       data={tracks}
--- src/providers/ApolloClientProvider.tsx
+++ src/providers/ApolloClientProvider.tsx
@@ -2,9 +2,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { PropsWithChildren } from "react";
 
 const client = new ApolloClient({
-  uri: "process.env.APOLLO_API_URI",
+  uri: "https://harar.stepzen.net/api/iced-elk/__graphql",
   headers: {
-    Authorization: "apikey ${process.env.APOLLO_API_KEY}",
+    Authorization:
+      "apikey harar::stepzen.net+1000::f0d347764f10f670fc5061e6451123a6e36ea2e943e1295ffc20c26aa7dced41",
   },
   cache: new InMemoryCache(),
 });
